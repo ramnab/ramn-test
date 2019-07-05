ed0f569f6de39f7e87556cd5371a332493fc4b4d
gavinchurcher-kcom <gavin.churcher@kcom.com>
2019-07-05 10:23:00 +0100

    corrected config file for gassafe; added for tradeuk

--- CAPITA/pipelines/modules/customer-baseline-pipeline/pipeline-deployer.sh
+++ CAPITA/pipelines/modules/customer-baseline-pipeline/pipeline-deployer.sh
@@ -16,15 +16,29 @@
 DEPT=$1
 ENV=$2
 CONFIG=$3
+MODULE=$4
 DIRECTORY=$(echo "$(dirname $0)/../../../new-customer-baseline")
 
 # Deploying module: call-recordings-bucket
-${DIRECTORY}/modules/call-recordings-bucket/pipeline-deployer.sh ${DEPT} ${ENV} ${CONFIG}
+if [[ ${MODULE} == 'all' || ${MODULE} == 'call-recordings-bucket' ]]; then
+    echo "Deploying call-recordings-bucket"
+    ${DIRECTORY}/modules/call-recordings-bucket/pipeline-deployer.sh ${DEPT} ${ENV} ${CONFIG}
+fi
 
 # Deploying module: baseline
-${DIRECTORY}/modules/customer-baseline/pipeline-deployer.sh ${DEPT} ${ENV} ${CONFIG}
+if [[ ${MODULE} == 'all' || ${MODULE} == 'customer-baseline' ]]; then
+    echo "Deploying customer baseline"
+    ${DIRECTORY}/modules/customer-baseline/pipeline-deployer.sh ${DEPT} ${ENV} ${CONFIG}
+fi
 
 # Deploying module: keys
-${DIRECTORY}/modules/keys/pipeline-deployer.sh ${DEPT} ${ENV} ${CONFIG}
+if [[ ${MODULE} == 'all' || ${MODULE} == 'keys' ]]; then
+    echo "Deploying keys"
+    ${DIRECTORY}/modules/keys/pipeline-deployer.sh ${DEPT} ${ENV} ${CONFIG}
+fi
 
-
+# Deploying module: cloudtrail
+if [[ ${MODULE} == 'all' || ${MODULE} == 'cloudtrail' ]]; then
+    echo "Deploying cloudtrail"
+    ${DIRECTORY}/modules/cloudtrail/pipeline-deployer.sh ${DEPT} ${ENV} ${CONFIG}
+fi
