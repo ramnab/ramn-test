a4ffdc2a89e746b84a8471bfca260b4c24c7a738
Amol Datir <amol.datir@capita.co.uk>
2019-08-14 08:47:24 +0000

    Added config.js

---
+++ CAPITA/reporting/web-interface/html/js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: "eu-central-1_zltnhEfl4",
+        userPoolClientId: "5gqp15pncup52u21m3uvinobm8",
+        region: "eu-central-1"
+    },
+    api: {
+        invokeUrl: 'https://c6ox1wjoy7.execute-api.eu-central-1.amazonaws.com/nonprod/variables'
+    },
+    env: "non-prod"
+};
