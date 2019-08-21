11163f2ffaae4232964836dbc1d78cac518f00e0
gavin.churcher <gavin.churcher@kcom.com>
2019-08-21 12:13:12 +0100

    updates to TradeUK specific stacks

---
+++ CAPITA/new-customer-baseline/modules/tradeuk/README.md
@@ -0,0 +1,12 @@
+# TradeUK Customer Baseline
+
+TradeUK is currently out of sync with the main new-customer-baseline solution.
+
+Currently, to maintain partial compatibility, only the module 'tradeuk' should be run into
+the accounts
+
+```bash
+cd new-customer-baseline
+./scripts/deploy.sh <ENV> tradeuk tradeuk
+
+```
--- CAPITA/new-customer-baseline/modules/tradeuk/deployer.sh
+++ CAPITA/new-customer-baseline/modules/tradeuk/deployer.sh
@@ -12,6 +12,16 @@ echo """
 
 """
 
+# look up appropriate key ID from alias
+#KEY_ALIAS=$(grep master_key ${DIRECTORY}/../../transforms/config-deployer.yml | awk '{print $2}')
+##KEY_ID=$(aws kms list-aliases --query "Aliases[?AliasName=='${KEY_ALIAS}'].TargetKeyId" --output text)
+##
+##echo "${KEY_ALIAS} => ${KEY_ID}"
+##
+##cat >> ${DIRECTORY}/../../transforms/config-deployer.yml <<EOL
+##kms_key_id: ${KEY_ID}
+##EOL
+
 cf sync -y --context ${DIRECTORY}/../../transforms/config-deployer.yml ${DIRECTORY}/tradeuk.stacks
 
 
