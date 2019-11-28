b35e59c492839bb6c334c9259d7030a16cf2fa4b
Amol Datir <Amol.Datir@capita.com>
2019-11-28 23:21:49 +0000

    Added the server side validations for the variable names and values. User Story - 1001.

--- CAPITA/reporting/web-interface/src/WebInterface_SetVariables/lambda_function.py
+++ CAPITA/reporting/web-interface/src/WebInterface_SetVariables/lambda_function.py
@@ -1,26 +1,32 @@
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 import json
 import os
 
 def lambda_handler(event, context):
     print ("The connect variable being updated is: " + json.dumps(event))
+    allowedValues = ["true","false", "0","10","20","30","40","50","60","70","80","90","100"]
 
     try:
-        tableName = os.environ["VariablesDB"]
-        dynamoDB = boto3.resource('dynamodb')
-        table = dynamoDB.Table(tableName)
-        
-        table.update_item(
-            TableName = tableName,
-            Key = {
-                'VariableName': event['variableName']
-            },
-            UpdateExpression = "SET VariableValue = :a",
-            ExpressionAttributeValues = {
-                ':a': event['variableValue']
-            }
-        )
-        return {"success":"Variable has been updated successfully"}
+        if event['variableValue'] in allowedValues:
+            tableName = os.environ["VariablesDB"]
+            dynamoDB = boto3.resource('dynamodb')
+            table = dynamoDB.Table(tableName)
+            
+            table.update_item(
+                TableName = tableName,
+                Key = {
+                    'VariableName': event['variableName']
+                },  
+                UpdateExpression = "SET VariableValue = :a",
+                ConditionExpression = Attr('VariableName').exists(),
+                ExpressionAttributeValues = {
+                    ':a': event['variableValue']
+                }
+            )
+            return {"success":"Variable has been updated successfully"}
+        else:
+            print ("The update request is failed. The value entered is " + event['variableValue'] + " which is not a valid Variable Value")
 
     except Exception as e:
         print ("Something went wrong when updating the records in the table")
